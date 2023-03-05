0ce32cedfe2c2c79435fdd62ca25d024deaa6a12
Maximilian Seidler <maximilian.seidler@student.tugraz.at>
2023-03-05 22:14:28 +0100

    Tortillas expect setup

--- userspace/libc/include/assert.h
+++ userspace/libc/include/assert.h
@@ -7,3 +7,8 @@
     printf("Assertion failed: '%s', file %s, function %s, line %d\n", #X, __FILE__, __FUNCTION__, __LINE__); \
  exit(-1); } } while (0)
 
+/*
+* Wrapper around printf. Everything, that gets printed with this gets
+* prefixed with 'TORTILLAS EXPECT: '.
+**/
+#define tortillas_expect(FMT, ...) printf("TORTILLAS EXPECT: " FMT, __VA_ARGS__);
